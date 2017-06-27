0d562a81612ffd85d3b9396894c34d80a14c8252
BoscoMurray <rossamurray@gmail.com>
2017-06-27 18:21:02 +0100

    cameras can print

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -5,11 +5,15 @@ import org.junit.Before;
 public class PhotographerTest {
   Camera camera01;
   Photographer photographer;
+  AnalogCamera acamera;
+  DigitalCamera dcamera;
 
   @Before
   public void before() {
     camera01 = new Camera("Nikon 34T");
     photographer = new Photographer();
+    acamera = new AnalogCamera();
+    dcamera = new DigitalCamera();
   }
 
   @Test
@@ -24,5 +28,16 @@ public class PhotographerTest {
     photographer.remove();
     assertEquals(0, photographer.cameraCount());
   }
+
+  @Test
+  public void analogCameraCanPrint() {
+    assertEquals("Nikon 34T, Noise reduction filter, Hi-resolution", acamera.printDetails());
+  }
+
+  @Test
+  public void digitalCameraCanPrint() {
+    assertEquals("Sony ILCE5100L, No noise reduciton, Lo-resolution", dcamera.printDetails());
+  }
+
 
 }
