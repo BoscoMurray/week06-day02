410a8df7ef234760d5afc6c4b70483070ea05e24
BoscoMurray <rossamurray@gmail.com>
2017-06-27 16:44:22 +0100

    initial commit

---
+++ Photographer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList
+
+public class Photographer {
+
+  private ArrayList<Camera> cameras;
+
+  public Photographer(String camera) {
+    this.cameras = new ArrayList<Camera>();
+  }
+
+
+  
+}
